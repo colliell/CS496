6e9d1c24a1ecfc25b3c4e79076facb7b55dc9a97
henzier <henzier@oregonstate.edu>
2016-05-01 18:53:31 -0700

    Revert "Updated prop object and implemented ranking"
    This reverts commit e53d175411fca7a56a60d69815fa68b0bd4e21b8.
    
    # Conflicts:
    
    #	Forrent/app/src/main/java/com/example/project/forrent/AddItemActivity.java
    
    #	Forrent/app/src/main/java/com/example/project/forrent/MainActivity.java
    #	Forrent/app/src/main/java/com/example/project/forrent/Prop.java
    #	Forrent/app/src/main/java/com/example/project/forrent/PropList.java

--- Forrent/app/src/main/java/com/example/project/forrent/DetailActivity.java
+++ Forrent/app/src/main/java/com/example/project/forrent/DetailActivity.java
@@ -1,19 +1,5 @@
 package com.example.project.forrent;
 
-<<<<<<< HEAD
-import android.app.Activity;
-import android.app.ListActivity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.TextView;
-=======
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -22,12 +8,9 @@ import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
->>>>>>> origin/master
-
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -39,16 +22,8 @@ public class DetailActivity extends AppCompatActivity {
     private List<Map<String, String>> data;
     private ListView listView2 = null;
 
-    private ListView listViewAFterEdit = null;
-    final static int EDIT_ITEM_INTENT=1;
-    Prop prop;
-    List<Prop> props = new ArrayList<Prop>(Arrays.asList(new Prop[]{}));
-    static final int List_Item = 1;
-    private String TAG="1";
 
     public void onCreate(Bundle savedInstanceState) {
-
->>>>>>> origin/master
         super.onCreate(savedInstanceState);
         String addr = getIntent().getStringExtra("addr");
         String link = getIntent().getStringExtra("link");
@@ -58,18 +33,6 @@ public class DetailActivity extends AppCompatActivity {
         String price = getIntent().getStringExtra("price");
         String sqft = getIntent().getStringExtra("sqft");
         String pets = getIntent().getStringExtra("pets");
-<<<<<<< HEAD
-        prop = new Prop(addr, link, rank, rooms, bathrooms
-                , price, sqft, pets);
-
-        listView2 = new ListView(this);
-        String[] from={"addr", "link", "rank", "rooms", "bathrooms"
-                , "price", "sqft", "pets"};
-        int[] to = {R.id.Addr, R.id.Link, R.id.rank, R.id.price
-                , R.id.sqft, R.id.rooms, R.id.bathrooms, R.id.pets};
-        SimpleAdapter adapter = new SimpleAdapter(getBaseContext(), data,
-                R.layout.detail_layout, from, to);
-=======
         String date = getIntent().getStringExtra("date");
         String phone = getIntent().getStringExtra("phone");
         String email = getIntent().getStringExtra("email");
@@ -84,12 +47,8 @@ public class DetailActivity extends AppCompatActivity {
                 new int[]{R.id.Addr, R.id.Link, R.id.price, R.id.rank
                         , R.id.rooms, R.id.bathrooms, R.id.sqft, R.id.pets, R.id.date,
                         R.id.phone, R.id.email});
->>>>>>> origin/master
         listView2.setAdapter(adapter);
         setContentView(listView2);
-        //setContentView(R.layout.detail_layout);
-
-
     }
 
     @Override
@@ -152,47 +111,6 @@ public class DetailActivity extends AppCompatActivity {
         hm.put("email", prop.getEmail());
         data.add(hm);
     }
-<<<<<<< HEAD
-
-
-    public void onClick(View v) {
-        //validateSaveExit();
-
-        Intent intent = new Intent(DetailActivity.this, EdititemActivity.class);
-
-        intent.putExtra("addr", prop.getAddr());
-        intent.putExtra("rank", prop.getRank());
-        intent.putExtra("link", prop.getLink());
-        intent.putExtra("rooms", prop.getRooms());
-        intent.putExtra("bathrooms", prop.getBathrooms());
-        intent.putExtra("price", prop.getPrice());
-        intent.putExtra("sqft", prop.getSqft());
-        intent.putExtra("pets", prop.getPets());
-        startActivity(intent);
-    }
-    public void onClickgoback(View v) {
-        validateReplaceExit();
-    }
-
-    static final int SAVE_EDIT_ITEM_INTENT = -1;
-    private void validateReplaceExit() {
-        Intent intent = new Intent(this, MainActivity.class);
-
-        intent.putExtra("link", ((TextView) findViewById(R.id.Link)).getText().toString());
-        intent.putExtra("addr", ((TextView) findViewById(R.id.Addr)).getText().toString());
-        intent.putExtra("rank", ((TextView) findViewById(R.id.rank)).getText().toString());
-        intent.putExtra("rooms", ((TextView) findViewById(R.id.rooms)).getText().toString());
-        intent.putExtra("bathrooms", ((TextView) findViewById(R.id.bathrooms)).getText().toString());
-        intent.putExtra("price", ((TextView) findViewById(R.id.price)).getText().toString());
-        intent.putExtra("sqft", ((TextView) findViewById(R.id.sqft)).getText().toString());
-        intent.putExtra("pets", ((TextView) findViewById(R.id.pets)).getText().toString());
-        startActivity(intent);
-        setResult(2, intent);
-        //Log.i(TAG, "My position is validateSaveExit~~~~~~~~~~"+SAVE_EDIT_ITEM_INTENT);
-        //finish();
-    }
-=======
->>>>>>> origin/master
 }
 
 
